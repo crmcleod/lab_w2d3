13ddfe5fa861eb3c8f42e55cf67a69bea086902f
Craig McLeod <mcleod.craig@outlook.com>
2020-05-20 11:10:06 +0100

    Spec setups

---
+++ specs/pub_spec.rb
@@ -0,0 +1,12 @@
+require('minitest/autorun')
+require('minitest/reporters')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative("../customer")
+require_relative("../drinks")
+require_relative("../pub")
+
+class TestPub < Minitest::Test
+
+end
